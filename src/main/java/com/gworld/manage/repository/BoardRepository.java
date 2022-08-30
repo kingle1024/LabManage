79f1fbcc31bbe2cebc72de796a6fa9d5746ae4fa
kingle1024 <teran1024@naver.com>
2022-08-31 08:20:57 +0900

    feat : 공지사항 리스트 구현, 테스트 코드 작성 Close #6

--- src/main/java/com/gworld/manage/repository/BoardRepository.java
+++ src/main/java/com/gworld/manage/repository/BoardRepository.java
@@ -1,2 +1,8 @@
-package com.gworld.manage.repository;public class BoardRepository {
+package com.gworld.manage.repository;
+
+import com.gworld.manage.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
 }
--- src/main/java/com/gworld/manage/service/BoardService.java
+++ src/main/java/com/gworld/manage/service/BoardService.java
@@ -1,2 +1,9 @@
-package com.gworld.manage.service;public class BoardService {
+package com.gworld.manage.service;
+
+import com.gworld.manage.model.Board;
+
+import java.util.List;
+
+public interface BoardService {
+    List<Board> list();
 }
