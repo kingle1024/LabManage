1dd41acc85ad6099699973e15a0254b6672be72f
kingle1024 <teran1024@naver.com>
2022-08-31 17:04:07 +0900

    feat : 공지사항 보기 기능 개발 Close #8

--- src/main/java/com/gworld/manage/repository/BoardRepository.java
+++ src/main/java/com/gworld/manage/repository/BoardRepository.java
@@ -2,7 +2,11 @@ package com.gworld.manage.repository;
 
 import com.gworld.manage.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-
+    Optional<Board> findById(long id);
 }
--- src/main/java/com/gworld/manage/service/NoticeServiceImpl.java
+++ src/main/java/com/gworld/manage/service/NoticeServiceImpl.java
@@ -1,19 +1,33 @@
 package com.gworld.manage.service;
 
 import com.gworld.manage.model.Board;
+import com.gworld.manage.model.BoardDto;
 import com.gworld.manage.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class BoardServiceImpl implements BoardService{
+public class NoticeServiceImpl implements NoticeService {
 
     private final BoardRepository boardRepository;
     @Override
     public List<Board> list() {
         return boardRepository.findAll();
+    }
+
+    @Override
+    public BoardDto detail(long id) {
+        Optional<Board> optionalBoard = boardRepository.findById(id);
+
+        if(optionalBoard.isEmpty()){
+            return null;
+        }
+        Board board = optionalBoard.get();
+
+        return BoardDto.of(board);
     }
 }
