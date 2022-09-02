02ca7c98b557c791c3c4700fbda04eafad0c16f0
kingle1024 <teran1024@naver.com>
2022-09-02 20:29:07 +0900

    Security 로그인 페이지 및 권한 추가

--- src/main/java/com/gworld/manage/configuration/SecurityConfiguration.java
+++ src/main/java/com/gworld/manage/configuration/SecurityConfiguration.java
@@ -1,15 +1,29 @@
 package com.gworld.manage.configuration;
 
+import com.gworld.manage.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final MemberService memberService;
+    @Autowired
+    public AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Bean
+    PasswordEncoder getPasswordEncoder(){return new BCryptPasswordEncoder();}
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,6 +35,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/**"
                 ).permitAll();
 
+        http.formLogin()
+                .loginPage("/member/login")
+                        .successHandler(authenticationSuccessHandler)
+                        .permitAll();
+        http.exceptionHandling()
+                .accessDeniedPage("/error/denied");
+
         super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService)
+                .passwordEncoder(getPasswordEncoder());
+        super.configure(auth);
     }
 }
