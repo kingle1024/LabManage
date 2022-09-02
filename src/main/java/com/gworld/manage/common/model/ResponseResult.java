045c7369db6d511cacc61182d7779ccac0e8355c
kingle1024 <teran1024@naver.com>
2022-09-02 12:42:25 +0900

    API Controller에서 response로 활용할 ResponseResult 추가

---
+++ src/main/java/com/gworld/manage/common/model/ResponseResult.java
@@ -0,0 +1,15 @@
+package com.gworld.manage.common.model;
+
+import lombok.Data;
+
+//@Data
+public class ResponseResult {
+    ResponseResultHeader header;
+    Object body;
+    public ResponseResult(boolean result, String message){
+        header = new ResponseResultHeader(result, message);
+    }
+    public ResponseResult(boolean result){
+        header = new ResponseResultHeader(result);
+    }
+}
