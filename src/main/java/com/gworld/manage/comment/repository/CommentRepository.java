6e5b236614e261eedb004225f2ce5dbb00b5cc98
kingle1024 <teran1024@naver.com>
2022-09-08 09:32:19 +0900

    refactor : 컬렉션을 반환하는 Spring Data JPA Repository 메서드는 null을 반환하지 않고 비어있는 컬렉션을 반환해주므로 Optional로 감싸서 반환할 필요가 없음!

--- src/main/java/com/gworld/manage/comment/repository/CommentRepository.java
+++ src/main/java/com/gworld/manage/comment/repository/CommentRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Optional<List<Comment>> findAllByBoardNoAndTypeCode(long boardNo, String typeCode);
+    List<Comment> findAllByBoardNoAndTypeCode(long boardNo, String typeCode);
 }
