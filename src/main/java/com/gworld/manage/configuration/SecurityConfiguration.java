def19009b0ac1d4150d58b80a7c12f84aee327d0
kingle1024 <teran1024@naver.com>
2022-09-13 09:10:18 +0900

    refactor : WebSecurityConfigurerAdapter is deprecated에 따른 구조 변환 작업 진행

--- src/main/java/com/gworld/manage/configuration/SecurityConfiguration.java
+++ src/main/java/com/gworld/manage/configuration/SecurityConfiguration.java
@@ -2,29 +2,27 @@ package com.gworld.manage.configuration;
 
 import com.gworld.manage.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    private final MemberService memberService;
+public class SecurityConfiguration{
     private final AuthenticationSuccessHandler authenticationSuccessHandler;
 
     @Bean
     PasswordEncoder getPasswordEncoder(){return new BCryptPasswordEncoder();}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         com.gworld.manage.configuration.AuthenticationSuccessHandler a = new com.gworld.manage.configuration.AuthenticationSuccessHandler();
         http.csrf().disable();
         http.headers().frameOptions().sameOrigin();
@@ -36,19 +34,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.formLogin()
                 .loginPage("/member/login")
-                        .successHandler(authenticationSuccessHandler)
-                        .successHandler(a)
-                        .permitAll();
+                .successHandler(authenticationSuccessHandler)
+                .permitAll();
         http.exceptionHandling()
                 .accessDeniedPage("/error/denied");
 
-        super.configure(http);
+        return http.build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(memberService)
-                .passwordEncoder(getPasswordEncoder());
-        super.configure(auth);
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
